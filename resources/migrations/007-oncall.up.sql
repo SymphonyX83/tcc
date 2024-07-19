1227dd19951cb5483851bd4f3c0fe86341c115aa
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2024-07-18 17:23:20 -0700

    fixed oncall table migration

--- resources/migrations/007-oncall.up.sql
+++ resources/migrations/007-oncall.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE oncall (
   rgroup_id int unsigned default 0,
   poncall_id int unsigned default 0 COMMENT "Select WHERE rgroup is the same than above selected",
   soncall_id int unsigned default 0 COMMENT "Select WHERE rgroup is the same than above selected",
-  start_date timestamp default null,
-  end_date timestamp default null
+  start_date date,
+  end_date date
 ) engine=innodb default charset=utf8; 
 
